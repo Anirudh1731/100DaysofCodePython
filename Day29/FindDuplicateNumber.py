248f287622b170597f93fda2ac5e0f58c34ec8bb
Anirudh Santhosh <80635397+Anirudh1731@users.noreply.github.com>
2024-03-22 22:05:44 +0530

    Create FindDuplicateNumber.py

---
+++ Day29/FindDuplicateNumber.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+
+        hashset=set()
+
+        for i in nums:
+            if(i in hashset):
+                return i
+            else:
+                hashset.add(i)
+
+        
+        
